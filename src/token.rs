297fc9514fa1a0d96b557d91abd5083b1d80f239
akthrms <akt.hrms@gmail.com>
2021-10-10 21:27:10 +0900

    update

--- src/token.rs
+++ src/token.rs
@@ -1,40 +1,67 @@
 #[derive(Debug, PartialEq)]
 pub enum Token {
+    /// 不正な文字
     Illegal(char),
+    /// 終端
     Eof,
 
     // 識別子 + リテラル
+    /// 文字列
     Ident(String),
+    /// 数値
     Int(isize),
 
     // 演算子
+    /// =
     Assign,
+    /// +
     Plus,
+    /// -
     Minus,
+    /// *
     Asterisk,
+    /// /
     Slash,
+    /// !
     Bang,
 
+    /// <
     Lt,
+    /// >
     Gt,
+    /// ==
     Eq,
+    /// !=
     Ne,
 
     // デリミタ
+    /// ,
     Comma,
+    /// ;
     Semicolon,
 
+    /// (
     LParen,
+    /// )
     RParen,
+    /// {
     LBrace,
+    /// }
     RBrace,
 
     // キーワード
+    /// fn
     Function,
+    /// let
     Let,
+    /// true
     True,
+    /// false
     False,
+    /// if
     If,
+    /// else
     Else,
+    /// return
     Return,
 }
