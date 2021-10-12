cb257ee44b0ab28e3ce533a539e881d0f30fd7e6
akthrms <akt.hrms@gmail.com>
2021-10-13 01:34:55 +0900

    update

--- src/ast.rs
+++ src/ast.rs
@@ -1,15 +1,37 @@
+use std::fmt;
+
 /// 文
 pub enum Statement {
     /// let
     LetStatement { name: String, value: Expression },
     /// return
     ReturnStatement(Expression),
+    /// 式
+    ExpressionStatement(Expression),
+}
+
+impl fmt::Display for Statement {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::LetStatement { name, value } => write!(f, "let {} = {};", name, value),
+            Self::ReturnStatement(expression) => write!(f, "return {};", expression),
+            Self::ExpressionStatement(expression) => write!(f, "{}", expression),
+        }
+    }
 }
 
 /// 式
 pub enum Expression {
     /// 文字列
     Identifier(String),
+}
+
+impl fmt::Display for Expression {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Expression::Identifier(value) => write!(f, "{}", value),
+        }
+    }
 }
 
 /// プログラム
