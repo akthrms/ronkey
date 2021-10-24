58e346cc061535df681bbdfb761c618d2ecda688
akthrms <akt.hrms@gmail.com>
2021-10-25 00:19:00 +0900

    update

--- src/lexer.rs
+++ src/lexer.rs
@@ -166,32 +166,28 @@ mod tests {
     #[test]
     fn test_next_token() {
         let input = r#"
-let five = 5;
-let ten = 10;
+        let five = 5;
+        let ten = 10;
+        let add = fn(x, y) {
+            x + y;
+        };
+        let result = add(five, ten);
+        !-/*5;
+        5 < 10 > 5;
+        if (5 < 10) {
+            return true;
+        } else {
+            return false;
+        }
+        10 == 10;
+        10 != 9;
+        "foobar";
+        "foo bar";
+        [1, 2];
+        {"foo": "bar"};
+        "#;
 
-let add = fn(x, y) {
-    x + y;
-};
-
-let result = add(five, ten);
-!-/*5;
-5 < 10 > 5;
-
-if (5 < 10) {
-    return true;
-} else {
-    return false;
-}
-
-10 == 10;
-10 != 9;
-"foobar";
-"foo bar";
-[1, 2];
-{"foo": "bar"};
-"#;
-
-        let tests = [
+        let expected_token = [
             Token::Let,
             Token::Identifier("five".to_string()),
             Token::Assign,
@@ -286,9 +282,9 @@ if (5 < 10) {
 
         let mut lexer = Lexer::new(input);
 
-        for test in tests.iter() {
+        for expected_token in expected_token {
             let token = lexer.next_token();
-            assert_eq!(token, *test);
+            assert_eq!(token, expected_token);
         }
     }
 }
